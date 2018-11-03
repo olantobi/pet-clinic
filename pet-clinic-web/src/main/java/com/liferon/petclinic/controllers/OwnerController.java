1e95e75b9b49f10e3c34d52e907f73582d85cf23
Ebenezer Olanrewaju <ebenezer.olanrewaju@venturegardengroup.com>
2018-11-03 10:05:44 +0100

    List Owners and Vets on their index page. Closes #12&#13

--- pet-clinic-web/src/main/java/com/liferon/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/liferon/petclinic/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package com.liferon.petclinic.controllers;
 
+import com.liferon.petclinic.model.Owner;
+import com.liferon.petclinic.services.OwnerService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owners")
+@AllArgsConstructor
 public class OwnerController {
 
+    private OwnerService ownerService;
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        Set<Owner> owners = ownerService.findAll();
+        model.addAttribute("owners", owners);
 
         return "owners/index";
     }
--- pet-clinic-web/src/main/java/com/liferon/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/liferon/petclinic/controllers/VetController.java
@@ -1,14 +1,26 @@
 package com.liferon.petclinic.controllers;
 
+import com.liferon.petclinic.model.Vet;
+import com.liferon.petclinic.services.VetService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/vets")
+@AllArgsConstructor
 public class VetController {
 
+    private VetService vetService;
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        Set<Vet> vets = vetService.findAll();
+        model.addAttribute("vets", vets);
 
         return "vets/index";
     }
